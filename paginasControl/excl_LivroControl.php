f97a5e816d34bb0159ce1c6ebc19c3a1d964802d
Lucas Rodrigues Rocha <lucas_3rocha@hotmail.com>
2022-06-06 20:00:44 -0300

    Alteracao no deletar livros
    Agora quando deleta um livro, tambem deleta suas avaliacoes

--- paginasControl/excl_LivroControl.php
+++ paginasControl/excl_LivroControl.php
@@ -3,18 +3,22 @@ session_start();
 include('conexao.php');
 include_once('verificaLogin.php');
 
+$sql = "SELECT * From stars";
+$resultado = mysqli_query($conexao, $sql);
+$arResultado = mysqli_fetch_assoc($resultado);
+
 //recupera dados
 $id = $_POST['id'];
 
-
-
-//alterar as informações
+//Deleta as informações
 $deleta = "DELETE FROM biblioteca WHERE idLivro = ". $id;
 
 //Envia as informações
 $resultado = mysqli_query($conexao, $deleta);
 
 if ($resultado) {
+	$deletaStars = "DELETE FROM stars WHERE idLivro = ". $id;
+	$resultadoStars = mysqli_query($conexao, $deletaStars);
 	header('Location: ../paginas/listaLivros.php');
 	exit();
 }else{
